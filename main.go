df89b67ca21abd69f223710343d1b7bfc7a19165
Sebastian Marines <sebastian0marines@gmail.com>
2023-06-24 17:15:23 -0600

    Export Prometheus metrics

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 )
@@ -11,6 +12,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.Path("/").HandlerFunc(HomeHandler)
+	router.Path("/metrics").Handler(promhttp.Handler())
 
 	fmt.Println("Server is running on port 8080")
 	err := http.ListenAndServe(":8080", router)
