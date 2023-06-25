f4704ebdfea77967197059298ab917f26a89512b
Sebastian Marines <sebastian0marines@gmail.com>
2023-06-25 00:56:24 -0600

    Create token decoder function

---
+++ tokens.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"strings"
+)
+
+type Token struct {
+	AccountId   int      `json:"accountId"`
+	RoleName    string   `json:"roleName"`
+	CompanyId   int      `json:"companyId"`
+	Username    string   `json:"UserName"`
+	User_name   string   `json:"user_name"`
+	Scope       []string `json:"scope"`
+	Exp         int      `json:"exp"`
+	Authorities []string `json:"authorities"`
+	Jti         string   `json:"jti"`
+	ClientId    string   `json:"client_id"`
+	SAID        []string `json:"SAID"`
+}
+
+func decodeToken(token string) Token {
+	payload := strings.Split(token, ".")[1]
+
+	decoded, err := base64.RawURLEncoding.DecodeString(payload)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var t Token
+	err = json.Unmarshal(decoded, &t)
+	if err != nil {
+		panic(err)
+	}
+
+	return t
+}
